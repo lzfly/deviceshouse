d36aa30cf1a077258c345fb25ec0ba84360ff1ba
luoiefly <luoiefly@163.com>
2017-11-22 08:32:59 +0800

    create project

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "robotserver/routers"
+
+	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/session/redis"
+)
+
+
+func handleSignals(c chan os.Signal) {
+	switch <-c {
+	case syscall.SIGINT, syscall.SIGTERM:
+		fmt.Println("Shutdown quickly, bye...")
+	case syscall.SIGQUIT:
+		fmt.Println("Shutdown gracefully, bye...")
+		// do graceful shutdown
+	}
+
+	os.Exit(0)
+}
+
+func main() {
+	//setUserID()
+
+	graceful, _ := beego.AppConfig.Bool("graceful")
+	if !graceful {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		go handleSignals(sigs)
+	}
+
+	beego.SetLogger("file", `{"filename":"logs/test.log"}`)
+	mode := beego.AppConfig.String("runmode")
+	if mode == "prod" {
+		beego.SetLevel(beego.LevelInformational)
+	}
+
+
+	beego.Run()
+}
---
+++ models/userbinddevices_io.go
@@ -0,0 +1,40 @@
+package models
+
+type UserBindDevicePostForm struct {
+	Username           string `json:"username"`
+	Device_sn          string `json:"device_sn"`
+}
+
+type UserBindDevicePostInfo struct {
+    Status   int    `json:"status"`
+	Code     int    `json:"code"`
+	UserBindDeviceInfo *UserBindDevice `json:"userbinddevice"`
+}
+
+type UserBindDeviceGetOneInfo struct {
+    Status   int    `json:"status"`
+	Code     int    `json:"code"`
+	UserBindDeviceInfo *UserBindDevice `json:"userbinddevice"`
+}
+
+type UserBindDeviceGetAllInfo struct {
+    Status   int    `json:"status"`
+	Code     int    `json:"code"`
+	UserBindDevicesInfo []UserBindDevice `json:"userbinddevices"`
+}
+
+type UserBindDevicePutForm struct {
+	Username           string  `json:"username"`
+	Device_sn          string `json:"device_sn"`
+}
+
+type UserBindDevicePutInfo struct {
+    Status   int    `json:"status"`
+	Code     int    `json:"code"`
+	UserBindDeviceInfo *UserBindDevice `json:"userbinddevice"`
+}
+
+type UserBindDeviceDeleteInfo struct {
+    Status   int    `json:"status"`
+	Code     int    `json:"code"`
+}
